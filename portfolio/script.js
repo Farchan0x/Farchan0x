2930244ea8bfcb70b9938b393680772f2c755efa
Farhan <Kennyyy897@gmail.com>
2026-01-30 19:00:44 +0700

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
+
+
+const name = document.getElementById("name").value;
+
+
+alert("Terima kasih " + name + ", pesan kamu terkirim!");
+
+
+this.reset();
+});
